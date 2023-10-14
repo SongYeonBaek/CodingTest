21292a60191fb7e4d54ae877ed1216cae9a16661
SongYeon <78153440+SongYeonBaek@users.noreply.github.com>
2023-10-15 02:43:09 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -8,24 +8,24 @@ int main()
     cin >> n;
     
     //배열 입력받기
-    int arr[301];
-    for(int i=1; i<=n; i++){
+    int arr[n];
+    for(int i=0; i<n; i++){
         cin >> arr[i];
     }
     
     //계단의 최댓값을 저장하기 위한 배열
-    int dp[301];
+    int dp[n];
     
-    dp[1] = arr[1]; //1번째 계단까지의 최댓값: 1번
-    dp[2] = arr[1] + arr[2]; //2번째 계단까지의 최댓값: 1번+2번 
-    dp[3] = max(arr[1]+arr[3], arr[2]+arr[3]); //3번째 계단까지의 최댓값: 1번+3번 or 2번+3번
+    dp[0] = arr[0]; //1번째 계단까지의 최댓값: 1번
+    dp[1] = arr[0] + arr[1]; //2번째 계단까지의 최댓값: 1번+2번 
+    dp[2] = max(arr[0]+arr[2], arr[1]+arr[2]); //3번째 계단까지의 최댓값: 1번+3번 or 2번+3번
     
     //4번째 계단부터는 점화식
-    for(int i=4; i<=n; i++){
+    for(int i=3; i<n; i++){
         dp[i] = max(dp[i-2] + arr[i], dp[i-3]+arr[i-1]+arr[i]);
     }
     
-    cout << dp[n] << "\n";
+    cout << dp[n-1] << "\n";
     
     return 0;
 }

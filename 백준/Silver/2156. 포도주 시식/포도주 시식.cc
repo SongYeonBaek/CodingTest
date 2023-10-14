3cc029f3e3b322fc0ff409797ca2e912493b1cad
SongYeon <78153440+SongYeonBaek@users.noreply.github.com>
2023-10-15 03:22:16 +0900

    [Silver I] Title: 포도주 시식, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    //배열 입력받기
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    
+    //계단의 최댓값을 저장하기 위한 배열
+    int dp[n];
+    
+    dp[0] = arr[0]; //1번째 계단까지의 최댓값: 1번
+    dp[1] = arr[0] + arr[1]; //2번째 계단까지의 최댓값: 1번+2번 
+    dp[2] = max({arr[0]+arr[2], arr[1]+arr[2], arr[0]+arr[1]}); //3번째 계단까지의 최댓값: 1번+3번 or 2번+3번 or 1번+2번
+    
+    //4번째 계단부터는 점화식
+    for(int i=3; i<n; i++){
+        dp[i] = max({dp[i-3]+arr[i-1]+arr[i], dp[i-2]+arr[i], dp[i-1]});
+    }
+    
+    cout << dp[n-1] << "\n";
+    
+    return 0;
+}

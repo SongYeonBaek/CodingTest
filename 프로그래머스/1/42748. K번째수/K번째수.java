04c58acc11cb345188486675c4e8ac189aee38b1
SongYeon <78153440+SongYeonBaek@users.noreply.github.com>
2024-05-13 21:13:55 +0900

    [level 1] Title: K번째수, Time: 0.21 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<Integer> solution(int[] array, int[][] commands) {
+        ArrayList<Integer> answer = new ArrayList<>();
+
+
+        for(int i=0; i<commands.length; i++){
+            ArrayList<Integer> list = new ArrayList<>();
+            for (int j = commands[i][0]-1; j < commands[i][1]; j++) {
+                list.add(array[j]);
+            }
+
+            Collections.sort(list);
+//            System.out.println(list);
+
+            answer.add(list.get(commands[i][2] - 1));
+        }
+
+        return answer;
+    }
+}

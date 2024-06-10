9104daa03b8dd8f0054c842b7ff2e983f747566c
SongYeon <78153440+SongYeonBaek@users.noreply.github.com>
2024-06-11 02:11:22 +0900

    [level 2] Title: 가장 큰 수, Time: 1.81 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,20 +1,24 @@
 import java.util.*;
 
 class Solution {
-    public String solution(int[] numbers) {
+public static String solution(int[] numbers) {
         String[] s = new String[numbers.length];
+
         for(int i=0; i< numbers.length; i++) {
-           s[i] = String.valueOf(numbers[i]);
+            s[i] = String.valueOf(numbers[i]);
         }
-        Arrays.sort(s, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                return (o2+o1).compareTo(o1+o2);
-            }
-        });
+
+        Arrays.sort(s, (o1, o2)-> (o2+o1).compareTo(o1+o2));
+
+//        Arrays.sort(s, new Comparator<String>(){
+//            @Override
+//            public int compare(String o1, String o2) {
+//                return (o1+o2).compareTo(o2+o1);
+//            }
+//        });
 
         if(s[0].equals("0")) return "0";
-        
+
         StringBuilder sb = new StringBuilder();
 
         for (String str : s) {

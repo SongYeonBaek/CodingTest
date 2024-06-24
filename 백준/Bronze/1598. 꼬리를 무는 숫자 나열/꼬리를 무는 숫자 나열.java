ff9d044283ef14b36b8c59caeeb74e6133b265ac
SongYeon <78153440+SongYeonBaek@users.noreply.github.com>
2024-06-25 01:05:27 +0900

    [Bronze III] Title: 꼬리를 무는 숫자 나열, Time: 120 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/1598. 꼬리를 무는 숫자 나열/꼬리를 무는 숫자 나열.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        int n = Integer.parseInt(input.split(" ")[0]) - 1;
+        int m = Integer.parseInt(input.split(" ")[1]) - 1;
+
+        System.out.println(Math.abs(m%4 - n%4) + Math.abs(n/4-m/4));
+    }
+}

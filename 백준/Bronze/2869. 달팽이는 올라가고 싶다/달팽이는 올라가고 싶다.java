1322bd8c74952b0d30070d4bf57deb6a90abb328
SongYeon <78153440+SongYeonBaek@users.noreply.github.com>
2024-06-25 15:31:25 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 112 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        int A = Integer.parseInt(input.split(" ")[0]);
+        int B = Integer.parseInt(input.split(" ")[1]);
+        int V = Integer.parseInt(input.split(" ")[2]);
+
+        int days = (V-B) / (A-B);
+       
+        if((V-B) % (A-B) != 0) days++;
+       
+        System.out.println(days);
+    }
+}

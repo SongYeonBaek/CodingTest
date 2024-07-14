a98a3f90f911ef5b2e2c5c959b1263dbce2d3f14
SongYeon <78153440+SongYeonBaek@users.noreply.github.com>
2024-07-14 23:31:48 +0900

    [Bronze II] Title: 치킨 쿠폰, Time: 1796 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/1673. 치킨 쿠폰/치킨 쿠폰.java
@@ -0,0 +1,33 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void chicken(int n , int k){
+        int chicken = 0;
+        //쿠폰이 k장 이상일 동안 반복
+        while(n >= k) {
+            chicken += k;
+            n -= k - 1;
+        }
+        chicken += n;
+        System.out.println(chicken);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        String input;
+        while((input = br.readLine()) != null) {
+            st = new StringTokenizer(input);
+
+            int n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+
+            chicken(n, k);
+        }
+    }
+}

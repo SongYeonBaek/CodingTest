78b5890a21bd0f82782ca65537a98c7bc23ae58c
SongYeon <78153440+SongYeonBaek@users.noreply.github.com>
2024-07-23 03:02:51 +0900

    [Silver IV] Title: 정수 제곱근, Time: 104 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Silver/2417. 정수 제곱근/정수 제곱근.java
+++ 백준/Silver/2417. 정수 제곱근/정수 제곱근.java
@@ -8,10 +8,35 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         long n = Long.parseLong(br.readLine());
 
-        long q = (long) Math.sqrt(n);
+//        1. Math.sqrt() 사용해서 푸는 방법
+//        long q = (long) Math.sqrt(n);
+//
+//        if((q*q) < n) q++;
+//
+//        System.out.println(q);
 
-        if((q*q) < n) q++;
+//        2. 이분 탐색으로 푸는 방법
+        long start = 0;
+        long end = n;
+        long result = 0;
 
-        System.out.println(q);
+        while(start <= end){
+            long mid = (start + end) / 2;
+
+            //줄여야하는 경우 : end가 mid-1이  되면 됨
+            if(n <= Math.pow(mid, 2)){
+                result = mid;
+                end = mid -1;
+            }
+            //늘려야하는 경우
+            else {
+                start = mid + 1;
+            }
+        }
+
+        System.out.println(result);
+
+
+
     }
 }

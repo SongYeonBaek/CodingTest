023a55f2c2829ab9242fae4f0ac00c2fc76ad9d8
SongYeon <78153440+SongYeonBaek@users.noreply.github.com>
2024-07-23 11:45:55 +0900

    [Silver I] Title: 기타 레슨, Time: 308 ms, Memory: 23524 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.java
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.java
@@ -4,58 +4,45 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int N, M;
-	static int[] lesson;
-	static int left, right;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		//ex) {1,2,3,4,5,6,7,8,9}
-		//강의 수 N , 블루레이 갯수 M , 각 강의의 길이 배열 lesson 초기화 
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		lesson = new int[N];
-		
-		st = new StringTokenizer(br.readLine()," ");
-		int sum = 0;
-		for (int i = 0; i < N; i++) {
-			lesson[i] = Integer.parseInt(st.nextToken());
-			sum += lesson[i]; 
-			left = Math.max(left, lesson[i]); //9
-		}
-		right = sum;  //45
-		
-		System.out.println(binSearch(left,right));
+        st = new StringTokenizer(br.readLine());
+        int[] lect = new int[N];
+        long sum = 0;
+        long low = 0;  // should start from 0
+        long high = 0;
+        
+        for (int i = 0; i < N; i++) {
+            lect[i] = Integer.parseInt(st.nextToken());
+            sum += lect[i];
+            low = Math.max(low, lect[i]); // should be the max element in the array
+        }
 
-	}
-	public static long binSearch(long left, long right) {
-		while(left <= right) {
-			long sum = 0;
-			long mid = (left+right)/2;
-			int count = 1;  
-			for (int i = 0; i < N; i++) {
-				sum += lesson[i];
-				if(sum > mid) {
-					sum = lesson[i];
-					count ++;
-				}
-			}
-			//필요한 블루레이 갯수가 M보다 작거나 같으면 
-			if(count <= M) {
-				right = mid - 1;
-			}
-			//총 필요한 블루레이 개수가 M보다 크다면 
-			else {
-				left = mid +1;
-			}
-		}
-		return left;
-		
-		
-		
-	}
+        high = sum; // this should be sum of all elements
+        while (low <= high) {
+            long mid = (low + high) / 2;
+            int count = 1;
+            long sumTime = 0;
 
+            for (int i = 0; i < N; i++) {
+                sumTime += lect[i];
+                if (sumTime > mid) {
+                    sumTime = lect[i];
+                    count++;
+                }
+            }
+
+            if (count <= M) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        System.out.println(low);
+    }
 }

4b382ad44e0f4c85cadd734568d2f36aa2e460aa
SongYeon <78153440+SongYeonBaek@users.noreply.github.com>
2024-10-05 00:37:49 +0900

    [level 2] Title: 가장 큰 수, Time: 227.48 ms, Memory: 142 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,29 +2,21 @@ import java.util.*;
 
 class Solution {
 public static String solution(int[] numbers) {
-        String[] s = new String[numbers.length];
+    String[] s = new String[numbers.length];
+    
+    for(int i = 0; i < numbers.length; i++){
+        s[i] = String.valueOf(numbers[i]);
+    }
 
-        for(int i=0; i< numbers.length; i++) {
-            s[i] = String.valueOf(numbers[i]);
-        }
-
-        Arrays.sort(s, (o1, o2)-> (o2+o1).compareTo(o1+o2));
-
-//        Arrays.sort(s, new Comparator<String>(){
-//            @Override
-//            public int compare(String o1, String o2) {
-//                return (o1+o2).compareTo(o2+o1);
-//            }
-//        });
-
-        if(s[0].equals("0")) return "0";
-
-        StringBuilder sb = new StringBuilder();
-
-        for (String str : s) {
-            sb.append(str);
-        }
-
-        return sb.toString();
+    Arrays.sort(s, (o1, o2) -> (o2+o1).compareTo(o1+o2));
+    
+    if(s[0].equals("0")) return "0";
+    
+    StringBuilder sb = new StringBuilder();
+    for (String a : s) {
+        sb.append(a);
+    }
+    
+    return sb.toString();
     }
 }

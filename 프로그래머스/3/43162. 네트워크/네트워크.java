75a6e3b3a684bc70f53511564d2671b5b7b1cfc0
SongYeon <78153440+SongYeonBaek@users.noreply.github.com>
2024-10-07 23:22:50 +0900

    [level 3] Title: 네트워크, Time: 0.74 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,26 +1,36 @@
 class Solution {
     static boolean[] visited;
-    public static void dfs(int[][] computers, int x){
-        visited[x] = true;
-
-        for (int i = 0; i < computers.length; i++) {
-            if(computers[x][i] == 1 && !visited[i]) {
-                dfs(computers, i);
+    
+    public boolean dfs(int n, int[][] computers, int now){
+        //방문 처리
+        visited[now] = true;
+        
+        boolean flag = false;
+        
+        for(int i=0; i<n; i++){
+            if(i != now && computers[now][i] == 1) {
+                if(visited[i] == false) {
+                    flag = true;
+                    dfs(n, computers, i);
+                }
             }
         }
-
+        
+        return flag;
     }
-    public static int solution(int n, int[][] computers) {
-        visited = new boolean[computers.length];
-
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            if(!visited[i]) {
-                dfs(computers, i);
-                count++;
+    
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        visited = new boolean[n];
+        
+        for(int i=0; i<n; i++) {
+            if(visited[i] == false) {
+                answer++;
+                dfs(n, computers, i);
+                
             }
         }
-
-        return count;
+        
+        return answer;
     }
 }

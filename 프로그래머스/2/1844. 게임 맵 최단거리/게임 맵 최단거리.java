dcf02370cb20376f458e6892bc1f7b0f01ed49b9
SongYeon <78153440+SongYeonBaek@users.noreply.github.com>
2024-10-08 13:31:45 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 13.26 ms, Memory: 53.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,51 +1,50 @@
 import java.util.*;
 
 class Solution {
-        static boolean[][] visited;
-    static int answer = -1;
-
-    public static void bfs(int[][] maps, int x, int y){
-        //현재 노드 방문 처리
-        visited[y][x] = true;
-
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0,0,-1, 1};
-
+    static int answer;
+    
+    public void bfs(int[][] maps){
+        int n = maps.length;
+        int m = maps[0].length;
+        
+        boolean[][] visited = new boolean[n][m];
+        
         Queue<Integer[]> q = new LinkedList<>();
-        //노드의 x, y, 총 움직인 거리
-        q.offer(new Integer[]{x, y, 1});
-
-        //현재 노드와 인접한 노드 중에서 map 범위 내에 있고, 길이 있는 노드를 찾아서 queue에 넣음
-        //queue가 빌 때까지 노드를 빼내고, bfs를 진행
+        visited[0][0] = true;
+        q.offer(new Integer[]{0, 0, 1});
+        
+        int[] dx = {0, 0, -1, 1};
+        int[] dy = {1, -1 , 0, 0};
+        
         while(!q.isEmpty()){
             Integer[] a = q.poll();
-
-            //도착점에 도달했으면
-            if(a[0] ==  maps[0].length-1 && a[1] == maps.length-1){
-                answer = a[2];
+            
+            if(a[0] == n-1 && a[1] == m-1) {
+                answer = Math.min(answer, a[2]);
                 break;
             }
-
-            int nx, ny;
-            for (int i = 0; i < 4; i++) {
-                nx = a[0] + dx[i];
-                ny = a[1] +dy[i];
-
-                if(nx < 0 || nx >= maps[0].length || ny < 0 || ny >= maps.length) continue;
-                if(maps[ny][nx] == 1 && !visited[ny][nx]) {
-                    visited[ny][nx] = true;
-                    q.offer(new Integer[]{nx, ny, a[2] +1});
+            
+            for(int i=0; i<4; i++){
+                int nx = dx[i] + a[0];
+                int ny = dy[i] + a[1];
+                
+                if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+                
+                if(maps[nx][ny] == 1 && visited[nx][ny] == false) {
+                    visited[nx][ny] = true;
+                    q.offer(new Integer[]{nx, ny, a[2]+1});
                 }
             }
+            
         }
+        
     }
-
-    public static int solution(int[][] maps) {
-        visited = new boolean[maps.length][maps[0].length];
-
-        bfs(maps, 0, 0);
-
-        return answer;
+    
+    public int solution(int[][] maps) {
+        answer = Integer.MAX_VALUE;
+        
+        bfs(maps);
+        
+        return answer == Integer.MAX_VALUE ? -1 : answer;
     }
-
 }
